232f01d8dcf688f7608a9685d42eae985ad267e0
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2022-01-26 16:51:31 +0530

    added about & checkout pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+            blanditiis quaerat iusto mollitia consectetur fugit commodi
+            molestias quo repudiandae veniam voluptates magni sit facilis
+            facere, beatae dolorum corporis dolorem cupiditate!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
